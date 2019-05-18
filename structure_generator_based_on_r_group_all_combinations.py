1c9cc828181d18fa8428dbd16de74151e581dda5
Hiromasa Kaneko <31059982+hkaneko1985@users.noreply.github.com>
2019-05-18 10:09:14 +0900

    Add files via upload

--- structure_generator_based_on_r_group_all_combinations.py
+++ structure_generator_based_on_r_group_all_combinations.py
@@ -6,40 +6,52 @@
 import numpy as np
 from numpy import matlib
 from rdkit import Chem
-from rdkit.Chem import AllChem
 
-bond_list = [Chem.rdchem.BondType.UNSPECIFIED, Chem.rdchem.BondType.SINGLE, Chem.rdchem.BondType.DOUBLE, Chem.rdchem.BondType.TRIPLE, Chem.rdchem.BondType.QUADRUPLE, Chem.rdchem.BondType.QUINTUPLE, Chem.rdchem.BondType.HEXTUPLE, Chem.rdchem.BondType.ONEANDAHALF, Chem.rdchem.BondType.TWOANDAHALF, Chem.rdchem.BondType.THREEANDAHALF, Chem.rdchem.BondType.FOURANDAHALF, Chem.rdchem.BondType.FIVEANDAHALF, Chem.rdchem.BondType.AROMATIC, Chem.rdchem.BondType.IONIC, Chem.rdchem.BondType.HYDROGEN, Chem.rdchem.BondType.THREECENTER, Chem.rdchem.BondType.DATIVEONE, Chem.rdchem.BondType.DATIVE, Chem.rdchem.BondType.DATIVEL, Chem.rdchem.BondType.DATIVER, Chem.rdchem.BondType.OTHER, Chem.rdchem.BondType.ZERO]
+bond_list = [Chem.rdchem.BondType.UNSPECIFIED, Chem.rdchem.BondType.SINGLE, Chem.rdchem.BondType.DOUBLE,
+             Chem.rdchem.BondType.TRIPLE, Chem.rdchem.BondType.QUADRUPLE, Chem.rdchem.BondType.QUINTUPLE,
+             Chem.rdchem.BondType.HEXTUPLE, Chem.rdchem.BondType.ONEANDAHALF, Chem.rdchem.BondType.TWOANDAHALF,
+             Chem.rdchem.BondType.THREEANDAHALF, Chem.rdchem.BondType.FOURANDAHALF, Chem.rdchem.BondType.FIVEANDAHALF,
+             Chem.rdchem.BondType.AROMATIC, Chem.rdchem.BondType.IONIC, Chem.rdchem.BondType.HYDROGEN,
+             Chem.rdchem.BondType.THREECENTER, Chem.rdchem.BondType.DATIVEONE, Chem.rdchem.BondType.DATIVE,
+             Chem.rdchem.BondType.DATIVEL, Chem.rdchem.BondType.DATIVER, Chem.rdchem.BondType.OTHER,
+             Chem.rdchem.BondType.ZERO]
 
-main_molecule = Chem.MolFromMolFile('main_structure.mol') 
-fragment_molecules = [molecule for molecule in Chem.SDMolSupplier('fragments.sdf') if molecule is not None]
+main_molecule = Chem.MolFromMolFile('main_structure.mol')
+fragment_molecules = [molecule for molecule in Chem.SmilesMolSupplier('fragments.smi',
+                                                                      delimiter='\t', titleLine=False)
+                      if molecule is not None]
+# fragment_molecules = [molecule for molecule in Chem.SDMolSupplier('fragments.sdf') if molecule is not None]
 number_of_fragment_molecules = len(fragment_molecules)
 
 # make adjacency matrix and get atoms for main molecule
 main_adjacency_matrix = Chem.rdmolops.GetAdjacencyMatrix(main_molecule)
 for bond in main_molecule.GetBonds():
-   main_adjacency_matrix[bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()] = bond_list.index(bond.GetBondType())
-   main_adjacency_matrix[bond.GetEndAtomIdx(), bond.GetBeginAtomIdx()] = bond_list.index(bond.GetBondType())
+    main_adjacency_matrix[bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()] = bond_list.index(bond.GetBondType())
+    main_adjacency_matrix[bond.GetEndAtomIdx(), bond.GetBeginAtomIdx()] = bond_list.index(bond.GetBondType())
 main_atoms = []
 for atom in main_molecule.GetAtoms():
-   main_atoms.append(atom.GetSymbol())
+    main_atoms.append(atom.GetSymbol())
 
 r_index_in_main_molecule_old = [index for index, atom in enumerate(main_atoms) if atom == '*']
 r_index_in_main_molecule_new = []
 main_atom_number = main_adjacency_matrix.shape[0]
 for index, r_index in enumerate(r_index_in_main_molecule_old):
-    main_atoms[-index-1], main_atoms[r_index] = main_atoms[r_index], main_atoms[-index-1]
-    tmp = main_adjacency_matrix[:, main_atom_number-index-1:main_atom_number-index].copy()
-    main_adjacency_matrix[:, main_atom_number-index-1:main_atom_number-index] = main_adjacency_matrix[:, r_index:r_index+1]
-    main_adjacency_matrix[:, r_index:r_index+1] = tmp
-    tmp = main_adjacency_matrix[main_atom_number-index-1:main_atom_number-index, :].copy()
-    main_adjacency_matrix[main_atom_number-index-1:main_atom_number-index, :] = main_adjacency_matrix[r_index:r_index+1, :]
-    main_adjacency_matrix[r_index:r_index+1, :] = tmp
-    r_index_in_main_molecule_new.append(len(main_atoms) - index - 1)
+    modified_index = r_index - index
+    atom = main_atoms.pop(modified_index)
+    main_atoms.append(atom)
+    tmp = main_adjacency_matrix[:, modified_index:modified_index + 1].copy()
+    main_adjacency_matrix = np.delete(main_adjacency_matrix, modified_index, 1)
+    main_adjacency_matrix = np.c_[main_adjacency_matrix, tmp]
+    tmp = main_adjacency_matrix[modified_index:modified_index + 1, :].copy()
+    main_adjacency_matrix = np.delete(main_adjacency_matrix, modified_index, 0)
+    main_adjacency_matrix = np.r_[main_adjacency_matrix, tmp]
+r_index_in_main_molecule_new = [index for index, atom in enumerate(main_atoms) if atom == '*']
 
 r_bonded_atom_index_in_main_molecule = []
 for number in r_index_in_main_molecule_new:
     r_bonded_atom_index_in_main_molecule.append(np.where(main_adjacency_matrix[number, :] != 0)[0][0])
-r_bond_number_in_main_molecule = main_adjacency_matrix[r_index_in_main_molecule_new, r_bonded_atom_index_in_main_molecule]
+r_bond_number_in_main_molecule = main_adjacency_matrix[
+    r_index_in_main_molecule_new, r_bonded_atom_index_in_main_molecule]
 
 main_adjacency_matrix = np.delete(main_adjacency_matrix, r_index_in_main_molecule_new, 0)
 main_adjacency_matrix = np.delete(main_adjacency_matrix, r_index_in_main_molecule_new, 1)
@@ -50,52 +62,63 @@ main_size = main_adjacency_matrix.shape[0]
 
 all_fragment_combinations = np.arange(number_of_fragment_molecules)
 all_fragment_combinations = np.reshape(all_fragment_combinations, (all_fragment_combinations.shape[0], 1))
-if len(r_index_in_main_molecule_new) > 1:    
+if len(r_index_in_main_molecule_new) > 1:
     for variable_number in range(2, len(r_index_in_main_molecule_new) + 1):
         grid_seed_tmp = matlib.repmat(np.arange(number_of_fragment_molecules), all_fragment_combinations.shape[0], 1)
-        all_fragment_combinations = np.c_[matlib.repmat(all_fragment_combinations, len(np.arange(number_of_fragment_molecules)), 1),
-                                np.reshape(grid_seed_tmp.T, (np.prod(grid_seed_tmp.shape), 1))]
+        all_fragment_combinations = np.c_[
+            matlib.repmat(all_fragment_combinations, len(np.arange(number_of_fragment_molecules)), 1),
+            np.reshape(grid_seed_tmp.T, (np.prod(grid_seed_tmp.shape), 1))]
 
-writer = Chem.SDWriter('generated_structures.sdf')
+generated_structures = []
 for generated_structure_number in range(all_fragment_combinations.shape[0]):
-    print(generated_structure_number+1, '/', all_fragment_combinations.shape[0])
     generated_molecule_atoms = main_atoms[:]
     generated_adjacency_matrix = main_adjacency_matrix.copy()
     for r_number_in_molecule in range(len(r_index_in_main_molecule_new)):
-        fragment_molecule = fragment_molecules[all_fragment_combinations[generated_structure_number, r_number_in_molecule]]
+        fragment_molecule = fragment_molecules[
+            all_fragment_combinations[generated_structure_number, r_number_in_molecule]]
         fragment_adjacency_matrix = Chem.rdmolops.GetAdjacencyMatrix(fragment_molecule)
         for bond in fragment_molecule.GetBonds():
-           fragment_adjacency_matrix[bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()] = bond_list.index(bond.GetBondType())
-           fragment_adjacency_matrix[bond.GetEndAtomIdx(), bond.GetBeginAtomIdx()] = bond_list.index(bond.GetBondType())
+            fragment_adjacency_matrix[bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()] = bond_list.index(
+                bond.GetBondType())
+            fragment_adjacency_matrix[bond.GetEndAtomIdx(), bond.GetBeginAtomIdx()] = bond_list.index(
+                bond.GetBondType())
         fragment_atoms = []
         for atom in fragment_molecule.GetAtoms():
-           fragment_atoms.append(atom.GetSymbol())
-        
+            fragment_atoms.append(atom.GetSymbol())
+
         # integrate adjacency matrix
         r_index_in_fragment_molecule = fragment_atoms.index('*')
-        
-        r_bonded_atom_index_in_fragment_molecule = np.where(fragment_adjacency_matrix[r_index_in_fragment_molecule, :] != 0)[0][0]
-        r_bond_number_in_fragment_molecule = fragment_adjacency_matrix[r_index_in_fragment_molecule, r_bonded_atom_index_in_fragment_molecule]
+
+        r_bonded_atom_index_in_fragment_molecule = \
+            np.where(fragment_adjacency_matrix[r_index_in_fragment_molecule, :] != 0)[0][0]
+        r_bond_number_in_fragment_molecule = fragment_adjacency_matrix[
+            r_index_in_fragment_molecule, r_bonded_atom_index_in_fragment_molecule]
         if r_bonded_atom_index_in_fragment_molecule > r_index_in_fragment_molecule:
             r_bonded_atom_index_in_fragment_molecule -= 1
-        
+
         fragment_atoms.remove('*')
         fragment_adjacency_matrix = np.delete(fragment_adjacency_matrix, r_index_in_fragment_molecule, 0)
-        fragment_adjacency_matrix = np.delete(fragment_adjacency_matrix, r_index_in_fragment_molecule, 1)    
-        
+        fragment_adjacency_matrix = np.delete(fragment_adjacency_matrix, r_index_in_fragment_molecule, 1)
+
         fragment_size = fragment_adjacency_matrix.shape[0]
-        
+
         main_size = generated_adjacency_matrix.shape[0]
-        generated_adjacency_matrix = np.c_[generated_adjacency_matrix, np.zeros([generated_adjacency_matrix.shape[0], fragment_adjacency_matrix.shape[0]], dtype='int32')]
-        generated_adjacency_matrix = np.r_[generated_adjacency_matrix, np.zeros([fragment_adjacency_matrix.shape[0], generated_adjacency_matrix.shape[1]], dtype='int32')]
-        
-        generated_adjacency_matrix[r_bonded_atom_index_in_main_molecule[r_number_in_molecule], r_bonded_atom_index_in_fragment_molecule + main_size] = r_bond_number_in_main_molecule[r_number_in_molecule]
-        generated_adjacency_matrix[r_bonded_atom_index_in_fragment_molecule + main_size, r_bonded_atom_index_in_main_molecule[r_number_in_molecule]] = r_bond_number_in_main_molecule[r_number_in_molecule]
+        generated_adjacency_matrix = np.c_[generated_adjacency_matrix, np.zeros(
+            [generated_adjacency_matrix.shape[0], fragment_adjacency_matrix.shape[0]], dtype='int32')]
+        generated_adjacency_matrix = np.r_[generated_adjacency_matrix, np.zeros(
+            [fragment_adjacency_matrix.shape[0], generated_adjacency_matrix.shape[1]], dtype='int32')]
+
+        generated_adjacency_matrix[r_bonded_atom_index_in_main_molecule[
+                                       r_number_in_molecule], r_bonded_atom_index_in_fragment_molecule + main_size] = \
+            r_bond_number_in_main_molecule[r_number_in_molecule]
+        generated_adjacency_matrix[
+            r_bonded_atom_index_in_fragment_molecule + main_size, r_bonded_atom_index_in_main_molecule[
+                r_number_in_molecule]] = r_bond_number_in_main_molecule[r_number_in_molecule]
         generated_adjacency_matrix[main_size:, main_size:] = fragment_adjacency_matrix
-        
+
         # integrate atoms
         generated_molecule_atoms += fragment_atoms
-               
+
     # generate structures 
     generated_molecule = Chem.RWMol()
     atom_index = []
@@ -111,13 +134,13 @@ for generated_structure_number in range(all_fragment_combinations.shape[0]):
                 continue
             else:
                 generated_molecule.AddBond(atom_index[index_x], atom_index[index_y], bond_list[bond])
-    
-    generated_molecule = generated_molecule.GetMol()
-#    print(Chem.MolToSmiles(generated_molecule))
-    number_of_generated_structures = 0
-    generated_molecule.UpdatePropertyCache(True)
-    AllChem.Compute2DCoords(generated_molecule)
-    writer.write(generated_molecule)
-writer.close()
 
-    
+    generated_molecule = generated_molecule.GetMol()
+    generated_structures.append(Chem.MolToSmiles(generated_molecule))
+    if (generated_structure_number + 1) % 1000 == 0 or (generated_structure_number + 1) == \
+            all_fragment_combinations.shape[0]:
+        print(generated_structure_number + 1, '/', all_fragment_combinations.shape[0])
+str_ = '\n'.join(generated_structures)
+with open('generated_structures.smi', 'wt') as writer:
+    writer.write(str_)
+writer.close()
